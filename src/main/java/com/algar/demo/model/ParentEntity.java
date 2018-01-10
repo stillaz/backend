7db28694ee69e3756cc72245c32269e23ef8da8a
Alexander Aguirre Zorrilla <aguirrealexorri@gmail.com>
2018-01-10 08:52:45 -0500

    Entity user created

---
+++ src/main/java/com/algar/demo/model/ParentEntity.java
@@ -0,0 +1,35 @@
+package com.algar.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5758157609271228787L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
